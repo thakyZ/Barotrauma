f3238b6e1d878a1a1994994b4191f55301df7d26
尚嘉宣 <34389517+shangjiaxuan@users.noreply.github.com>
2023-04-14 23:34:46 +0800

    Fix uintidentfier == 0 breaking network sync.

--- Barotrauma/BarotraumaShared/SharedSource/Prefabs/PrefabCollection.cs
+++ Barotrauma/BarotraumaShared/SharedSource/Prefabs/PrefabCollection.cs
@@ -369,6 +369,19 @@ namespace Barotrauma
             return null;
         }
 
+        private T? FindInternal(Predicate<T> predicate)
+        {
+            Prefab.DisallowCallFromConstructor();
+            foreach (var kpv in prefabs)
+            {
+                if (kpv.Value.activePrefabInternal is T p && predicate(p))
+                {
+                    return p;
+                }
+            }
+            return null;
+        }
+
         /// <summary>
         /// Returns true if a prefab with the given identifier exists, false otherwise.
         /// </summary>
@@ -474,9 +487,9 @@ namespace Barotrauma
 				(((prefab) => {
 					if (prefab is PrefabWithUintIdentifier prefabWithUintIdentifier)
 					{
-						if (!selector.isEmptyInternal)
+						if (!selector.isEmptyInternal && selector.activePrefabInternal_NoCreate != null && (selector.activePrefabInternal_NoCreate as PrefabWithUintIdentifier)!.UintIdentifier != 0)
 						{
-							prefabWithUintIdentifier.UintIdentifier = (selector.activePrefabInternal as PrefabWithUintIdentifier)!.UintIdentifier;
+							prefabWithUintIdentifier.UintIdentifier = (selector.activePrefabInternal_NoCreate as PrefabWithUintIdentifier)!.UintIdentifier;
 						}
 						else
 						{
@@ -486,7 +499,7 @@ namespace Barotrauma
 
 								//it's theoretically possible for two different values to generate the same hash, but the probability is astronomically small
 								T? findCollision()
-									=> Find(p =>
+									=> FindInternal(p =>
 										p.Identifier != prefab.Identifier
 										&& p is PrefabWithUintIdentifier otherPrefab
 										&& otherPrefab.UintIdentifier == prefabWithUintIdentifier.UintIdentifier);
